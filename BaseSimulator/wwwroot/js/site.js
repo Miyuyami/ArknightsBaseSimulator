5eae7cf3ac85fbd8b4589b8c808b43ebcb4d6c42
Miyuyami <tenshi.tenshi.angel@gmail.com>
2020-02-09 04:58:59 +0200

    add carousel event javascript

--- BaseSimulator/wwwroot/js/site.js
+++ BaseSimulator/wwwroot/js/site.js
@@ -17,3 +17,15 @@ function SetOnBootstrapSwitch(elem, funcCallback) {
 function OnSwitchChangeBootstrapSwitch(event, state) {
     OnBootstrapSwitchFunctionCallbackName.invokeMethodAsync("OnSwitchChange", state)
 }
+
+var OnCarouselSlidCallbackName;
+function SetSlidCarousel(elem, funcCallback) {
+    $(elem).off("slid.bs.carousel", OnCarouselSlid);
+    $(elem).on("slid.bs.carousel", OnCarouselSlid);
+
+    OnCarouselSlidCallbackName = funcCallback;
+}
+
+function OnCarouselSlid(e) {
+    OnCarouselSlidCallbackName.invokeMethodAsync("OnCarouselSlid", e.to)
+}
