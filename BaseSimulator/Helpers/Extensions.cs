91ba32a86ce50c01ebfedcd471ee2d14a670b9a1
Miyuyami <tenshi.tenshi.angel@gmail.com>
2020-02-10 04:46:42 +0200

    moved to more general table info for building new room

---
+++ BaseSimulator/Helpers/Extensions.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Arknights.BaseSimulator
+{
+    public static class Extensions
+    {
+        public static MarkupString ToMarkup(this string str) =>
+            new MarkupString(str);
+
+        public static (MarkupString, MarkupString) ToMarkup(this (object, object) tuple) =>
+            (tuple.Item1.ToString().ToMarkup(), tuple.Item2.ToString().ToMarkup());
+    }
+}
