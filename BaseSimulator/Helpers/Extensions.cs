6ecda3eabfbec26b4fbef8637d777dfbd7f099c6
Miyuyami <tenshi.tenshi.angel@gmail.com>
2020-02-10 21:24:27 +0200

    moved from specific to general requirements table

--- BaseSimulator/Helpers/Extensions.cs
+++ BaseSimulator/Helpers/Extensions.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Components;
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Components;
 
 namespace Arknights.BaseSimulator
 {
@@ -9,5 +11,13 @@ namespace Arknights.BaseSimulator
 
         public static (MarkupString, MarkupString) ToMarkup(this (object, object) tuple) =>
             (tuple.Item1.ToString().ToMarkup(), tuple.Item2.ToString().ToMarkup());
+
+        public static IEnumerable<IEnumerable<T>> Transpose<T>(this IEnumerable<IEnumerable<T>> src)
+        {
+            return src.SelectMany(inner => inner.Select((item, index) => new { item, index }))
+                      .GroupBy(i => i.index, i => i.item)
+                      .Select(g => g.ToList())
+                      .ToList();
+        }
     }
 }
